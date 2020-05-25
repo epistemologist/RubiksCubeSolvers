509b8c7184043a19649f2a431d0229e7e534f7db
Husnain Raza <hhrraazzaa@gmail.com>
2020-05-25 16:08:21 -0500

    Starting Thistlethwaite solver

---
+++ thistlethwaite.py
@@ -0,0 +1,17 @@
+from brute_force import *
+import random
+
+"""Implementation of the Thistlethwaite algorithm"""
+
+solver = BruteForceSolver("3x3.def", None, False)
+
+"""
+STEP 1: Reduction from group G = <L,R,F,B,U,D> to G1 = <L2,R2,F,B,U,D>
+This is equivalent to applying moves until the edge orientations = [0,0,...,0]
+"""
+
+
+
+def step1(cube):
+	pass
+
